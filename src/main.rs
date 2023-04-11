18f8202b95c3dde89c710ef0c7ff75a76276d977
Arthur Chern <weirong.cwr@gmail.com>
2023-04-11 16:54:23 +0800

    partial commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::string::String;
+// use bytes::Buf;
 fn main() {
     println!("Hello, world!");
 }
@@ -13,8 +14,23 @@ pub struct Options {
     segment_size: i32, //MByte
 }
 
+#[derive(Debug)]
+pub struct WALFile {}
+
+/// The encoding of blocks is largely borrowed from LevelDB's/RocksDB's write ahead log. Log file
+/// consists of a sequence of variable length records. Records are grouped by BlockSize(32k).
+///       +-----+-------------+--+----+----------+------+-- ... ----+
+/// File  | r0  |        r1   |P | r2 |    r3    |  r4  |           |
+///       +-----+-------------+--+----+----------+------+-- ... ----+
+///       <---- BlockSize ------>|<--- BlockSize ------>|
+#[derive(Debug)]
+pub struct Block {}
+
+#[derive(Debug)]
+pub struct Record {}
+
 pub trait LocalWAL {
-    fn new(path: &str)->Self;
+    fn new(path: &str) -> Self;
     fn check_exist();
     fn create_dir();
     fn truncate();
@@ -37,25 +53,50 @@ pub struct LocalFileWALImpl {
     abs_path: String,
 }
 
-
 impl LocalWAL for LocalFileWALImpl {
-
-    fn new(path: &str)->Self {
-        LocalFileWALImpl { abs_path: String::new()}
+    fn new(path: &str) -> Self {
+        LocalFileWALImpl {
+            abs_path: String::new(),
+        }
     }
 
-    fn check_exist() {
+    fn check_exist() {}
 
-    }
+    fn create_dir() {}
 
-    fn create_dir() {
+    fn truncate() {}
 
-    }
+    fn write() {}
 
-    fn truncate() {
-    }
+    fn read(seqID: i64) {}
+
+    fn open() {}
+
+    fn close() {}
+
+    fn first_seq() {}
+
+    fn last_seq() {}
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use bytes::Buf;
+
+    #[test]
+    fn test_buffer() {
+        let mut buf = &b"hello world"[..];
+
+        println!("default value: {:?}", buf);
+
+        assert_eq!(b'h', buf.get_u8());
+        assert_eq!(b'e', buf.get_u8());
+        assert_eq!(b'l', buf.get_u8());
+
+        let mut rest = [0; 8];
+        buf.copy_to_slice(&mut rest);
+
+        assert_eq!(&rest[..], &b"lo world"[..]);
+    }
 }
